1fd7e40842c75f84cc5e7834aeba979a1ba32441
Zaid <zaidjan1295@gmail.com>
2020-05-14 18:39:12 +0530

    downlaoded placed

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ const dotenv = require("dotenv")
 const axios = require("axios")
 const fs = require("fs")
 dotenv.config();
-
+const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 const app = express();
-const router = express.Router();
 
 app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8000);
@@ -24,41 +23,47 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-(async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto('https://m.starmakerstudios.com/d/playrecording?app=sm&from_user_id=6755399375119253&is_convert=true&recordingId=6755399308749310&share_type=whatsapp');
-    await page.waitFor("source")
-    const downloadLink = await page.evaluate(() => {
-      return document.querySelector("source").getAttribute("src")
-    })
-    axios({
-      method: "get",
-      url: downloadLink,
-      responseType: "stream"
-    })
-    .then(function (response) {
-      response.data.pipe(fs.createWriteStream("master.mp4"));
-      console.log("downloaded")
-    });
-})();
+// (async () => {
+//     const browser = await puppeteer.launch({headless: false});
+//     const page = await browser.newPage();
+//     await page.goto('https://m.starmakerstudios.com/d/playrecording?app=sm&from_user_id=6755399375119253&is_convert=true&recordingId=6755399308749310&share_type=whatsapp');
+//     await page.waitFor("source")
+//     const downloadLink = await page.evaluate(() => {
+//       return document.querySelector("source").getAttribute("src")
+//     })
+//     axios({
+//       method: "get",
+//       url: downloadLink,
+//       responseType: "stream"
+//     })
+//     .then(function (response) {
+//       response.data.pipe(fs.createWriteStream("master.mp4"));
+//       console.log("downloaded")
+//     });
+// })();
+
+app.post('/messageReceived', (req, res) => {
+  console.log("request", req)
+})
+app.post('/messageSent', (req, res) => {
+  console.log("request", req)
+
+})
 
 
-// const products = await page.evaluate(() => {
-//     const links = Array.from(document.querySelectorAll('.s-result-item'));
-//     return links.map(link => {
-//       if (link.querySelector(".a-price-whole")) {
-//         return {
-//           name: link.querySelector(".a-size-medium.a-color-base.a-text-normal").textContent,
-//           url: link.querySelector(".a-link-normal.a-text-normal").href,
-//           image: link.querySelector(".s-image").src,
-//           price: parseFloat(link.querySelector(".a-price-whole").textContent.replace(/[,.]/g, m => (m === ',' ? '.' : ''))),
-//         };
-//       }
-//     }).slice(0, 5);
-//   });
-  
-//   console.log(products.sort((a, b) => {
-//     return a.price - b.price;
-//   }));
-  
+client
+  .messages
+  .create({
+     from: 'whatsapp:+14155238886',
+     body: `It's taco time!`,
+     to: 'whatsapp:+919886406126'
+   })
+  .then(message => console.log(message.sid))
+  .catch(err => {
+    throw(err)
+  })
+
+
+  app.listen(port = 8000, () => {
+    console.log("serevr started at port", port)
+  })
